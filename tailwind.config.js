21d101f9aab4d7b1f45729e1972cf96e7e86cf78
Numa Tapmaden Limbu <nume@Numas-MacBook-Air.local>
2025-09-06 20:00:53 +0545

    Deployment

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        abhaya: ['"Abhaya Libre"', 'serif'],
+        poppins: ['Poppins', 'sans-serif'],
+         roboto: ['Roboto', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
